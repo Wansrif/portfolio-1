2f8dd3c263b1a342e6639ded2aad7c19cf017204
Wansrif <71681596+Wansrif@users.noreply.github.com>
2021-03-04 14:23:39 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// Event pada saat link di klik
+$('.page-scroll').on('click', function(e){
+    
+    // ambil isi href
+    var tujuan = $(this).attr('href');
+    // tangkap elemen
+    var elemenTujuan = $(tujuan);
+
+
+    // pindahkan scroll
+    // $('html, body').scrollTop(elemenTujuan.offset().top);
+    $('html, body').animate({
+        scrollTop: elemenTujuan.offset().top - 50
+    }, 1250, 'easeInOutExpo');
+
+    e.preventDefault();
+
+});
